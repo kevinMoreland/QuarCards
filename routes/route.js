69df3f2bd3c231d96d78e6db30d06a363703f283
ANGELLOPARR <angellop123@gmail.com>
2020-05-03 09:09:43 -0700

    update API call to get random card

--- routes/route.js
+++ routes/route.js
@@ -17,12 +17,30 @@ router.get('/cards', async (req, res, next)=>{
 
 router.get('/cardByNum/:num', async (req, res, next)=>{
     try{
-        var card = await Cards.findOne({'card_num': req.params.num});
+        let cardNum = console.log(req.params.num);
+        var card = await Cards.findOne({'card_num': cardNum});
         res.send(card);
     }
     catch(error) {
         res.send({error: error.message})
     }
 });
+
+router.get('/randomCard/:roomCode', async (req, res, next) => {
+    try {
+        let roomCode = req.params.roomCode.toUpperCase();
+        var sData = sessions[roomCode];
+        var nextCardIndex = Math.round(Math.random() * sData.activeCards.length);
+        var nextCardNum = sData.activeCards[nextCardIndex];
+        sData.activeCards.splice(nextCardIndex, 1);
+        sData.turnCards.push(nextCardNum);
+        console.log(sData.turnCards);
+        var card = await Cards.findOne({'card_num': nextCardNum});
+        res.send(card);
+    }
+    catch (error) {
+        res.send({'error': error.message})
+    }
+})
 
 module.exports = router;
