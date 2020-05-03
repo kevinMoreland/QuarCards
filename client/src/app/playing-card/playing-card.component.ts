65c1f5429d8fce980eaef882bf5d905c59c48e50
ANGELLOPARR <angellop123@gmail.com>
2020-05-03 09:10:36 -0700

    frontend for random card text

--- client/src/app/playing-card/playing-card.component.ts
+++ client/src/app/playing-card/playing-card.component.ts
@@ -4,6 +4,7 @@ import { CardService } from '../card.service';
 import { SocketService } from '../socket.service';
 import Card from 'src/entity/Card';
 import { cardMode } from '../../entity/data-structures/card-modes';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -17,12 +18,15 @@ export class PlayingCardComponent implements OnInit {
 
   @Input() currMode : cardMode;
   @Input() votingPhraseText : string;
+  @Input() numPlayers : number;
   @ViewChild("cardBackText", {read: ElementRef}) cardText: ElementRef;
   @ViewChild("cardFlipper", {read: ElementRef}) card: ElementRef;
   @ViewChild("choiceButton", {read: ElementRef}) choiceButton: ElementRef;
+  cardData : Card = new Card(null);
 
   cardFlipTime : number = 600;
   testCard : Card;
+  backendCardSubscription: Subscription;
 
   constructor(private cardService: CardService,
               private socketService: SocketService) { }
@@ -31,7 +35,18 @@ export class PlayingCardComponent implements OnInit {
   }
   ngAfterViewInit(): void {
     //this is in ngAfterViewInit because the card ElementRef must first be loaded
-    this.transitionToNewMode();
+    if (this.currMode != cardMode.myTurn) {
+      console.log('here');
+      this.transitionToNewMode();
+    }
+    else {
+      console.log('afterviewinit')
+      this.cardService.getRandomCard().subscribe( (cData) => {
+        console.log('card stuff');
+        this.cardData = cData;
+        this.cardText.nativeElement.textContent = cData.card_text;
+      });
+    }
   }
 
   //when the mode changes (players turn to pick card or vote  ), notify the card so it can transition to the new mode
@@ -41,8 +56,15 @@ export class PlayingCardComponent implements OnInit {
     }
   }
 
+  ngOnDestroy() {
+    if (this.backendCardSubscription) {
+      this.backendCardSubscription.unsubscribe();
+    }
+  }
+
   //modify the text and flip the cards as necessary to enter the new mode
   transitionToNewMode() : void {
+    console.log('transition');
     if(this.card != null){
       switch(this.currMode){
         case cardMode.voting: {
@@ -63,10 +85,16 @@ export class PlayingCardComponent implements OnInit {
           break;
         }
         case cardMode.myTurn: {
+          console.log('my turn');
           this.cardText.nativeElement.textContent = "";
           this.transitionCardToFace("back");
           
           //TODO: randomly select a card
+          this.cardService.getRandomCard().subscribe( (cData) => {
+            console.log('card stuff');
+            this.cardData = cData;
+            this.cardText.nativeElement.textContent = cData.card_text;
+          });
           this.cardText.nativeElement.textContent = "randomly selected card text";
           break;
         }
@@ -89,6 +117,7 @@ export class PlayingCardComponent implements OnInit {
   //flip a card over to the desired face side
   transitionCardToFace(face : String) : void {
     let cardIsOnFace : Boolean = this.card.nativeElement.classList.contains('flip');
+    console.log(cardIsOnFace);
     
     //switch statement on which card face we would like to end up at after some fancy flipping
     switch(face){
@@ -100,8 +129,11 @@ export class PlayingCardComponent implements OnInit {
         else { this.card.nativeElement.classList.toggle('flip'); }
         break;
       }
+      // NOTE: setTimeout is an asynchronous function, so toggling nativeElement outside of
+      // a setTimeout will always execute faster regardless of position in code
       case("back"): {
         if(!cardIsOnFace) {
+          console.log('here');
           setTimeout(() => this.card.nativeElement.classList.toggle('flip'), this.cardFlipTime);
           this.card.nativeElement.classList.toggle('flip');
         }
@@ -117,8 +149,21 @@ export class PlayingCardComponent implements OnInit {
     }
   }
 
-  onCardPicked() : void{
-    console.log("card picked: " + this.cardText.nativeElement.textContent);
-    this.socketService.pickCard(this.cardText.nativeElement.textContent);
+  onCardPicked() : void {
+    console.log(this.numPlayers);
+    if (this.numPlayers > 0) {
+      console.log("card picked: " + this.cardText.nativeElement.textContent);
+      this.socketService.pickCard(this.cardData);
+    }
+    else {
+      // The alert popup is in the parent component, and there's no easy way to trigger
+      // it from this component. Thus to prevent someone from picking a card when there's only
+      // one person in the lobby, we just pass this function null.
+      // The null is handled in the backend to not do any meaningful work, and when that
+      // gets back to the frontend and parent component through the socket service,
+      // it can still trigger the alert popup.
+      this.socketService.pickCard(null);
+    }
   }
+
 }
